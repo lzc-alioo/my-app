ce996280dbbd754b8175dceddc89d0cbb84780f8
lzc.java@icloud.com <Lzc@123.com>
2021-10-04 15:17:08 +0800

    已经解决了配置文件了

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 
-myserver_dev="192.168.16.233:8081"
-myserver_prod="192.168.16.198:8081"
-
-sed -i "s/$myserver_dev/$myserver_prod/g" src/*.js
+#myserver_dev="192.168.16.233:8081"
+#myserver_prod="192.168.16.198:8081"
+#
+#sed -i "s/$myserver_dev/$myserver_prod/g" src/*.js
 
 yarn build
 
 
-sed -i "s/$myserver_prod/$myserver_dev/g" src/*.js
+#sed -i "s/$myserver_prod/$myserver_dev/g" src/*.js
 
 
 path_work=/runtime/work
